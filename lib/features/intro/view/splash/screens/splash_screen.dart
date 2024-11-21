3425ae790c2adf2e9c9e8ed5c5e61f319f42ecf8
Ghaith Fuad <ghaith.fuad@outlook.com>
2024-11-21 06:32:52 +0300

    Add Introduction Screen
    1. Add 'smooth_page_indicator' package to show the current page in the Introduction screen.
    2. Splash Files moved to a folder to make things organized.

--- lib/features/intro/view/splash/screens/splash_screen.dart
+++ lib/features/intro/view/splash/screens/splash_screen.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:quran/features/intro/view/splash/widgets/splash_background.dart';
-import 'package:quran/features/intro/view/splash/widgets/splash_icon.dart';
+import 'package:quran/features/intro/view/splash/screens/intro_screen.dart';
+import 'package:quran/features/intro/view/splash/widgets/splash/splash_background.dart';
+import 'package:quran/features/intro/view/splash/widgets/splash/splash_icon.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
-  /* TODO: Introduction Screen */
   void whenSplashComplete(BuildContext context) {
-    Widget goToScreen = const Placeholder();
+    Widget goToScreen = const IntroScreen();
     MaterialPageRoute pageRoute = MaterialPageRoute(builder: (context) => goToScreen);
     Navigator.of(context).pushReplacement(pageRoute);
   }
