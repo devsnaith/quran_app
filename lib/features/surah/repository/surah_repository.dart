9777f4d0f3afd646627d2abde67fbd8cb78d0cb8
Ghaith Fuad <ghaith.fuad@outlook.com>
2024-12-09 22:28:31 +0300

    More Surahs Added!

--- lib/features/surah/repository/surah_repository.dart
+++ lib/features/surah/repository/surah_repository.dart
@@ -6,8 +6,12 @@ import 'package:quran/features/surah/models/surah_model.dart';
 class SurahRepository {
   Map<int, List<int>> surahPagesNumber = {
     1: [1],
+    8: [177, 178, 179, 180, 181, 182, 183, 184, 185, 186],
+    20: [312, 313, 314, 315, 316, 317, 318, 319, 320, 321],
+    34: [428, 429, 430, 431, 432, 433, 434],
+    35: [434, 435, 436, 437, 438, 439, 440],
+    36: [440, 441, 442, 443, 444, 445],
     76: [578, 579, 580],
-    8: [177, 178, 179, 180, 181, 182, 183, 184, 185, 186]
   };
 
   Future<List<dynamic>> getSurahsAsJson() async {
